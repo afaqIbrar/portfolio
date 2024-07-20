7fc6c88222bbb4d650cb77bb39e835b450b95095
afaqibrar <afaqibrar468@gmail.com>
2024-07-20 18:51:39 +0500

    Background Effect Button and Texts in Hero Component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,16 +1,49 @@
 import React from 'react'
 import { Spotlight } from './ui/Spotlight'
+import { BackgroundBeams } from './ui/BackgroundBeams'
+import { TextGenerateEffect } from './ui/TestGenerateEffect'
+import ShimmerButton from './ui/ShimmerButton'
+import { FaArrowDown } from 'react-icons/fa'
 
 const Hero = () => {
-  return (
-    <div className='pb-20 pt-36'>
-        <div>
-            <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen' fill='white'/>
-            <Spotlight className='top-10 left-full h-[80vh] w-[50vh]' fill='purples'/>
-            <Spotlight className='top-28 left-80 h-[80vh] w-[50vh]' fill='blue'/>
+    return (
+        <div className='pb-20 pt-36'>
+            <div>
+                <Spotlight
+                    className="-top-40 -left-20 md:-left-32 md:-top-20 h-screen"
+                    fill="white"
+                />
+                <Spotlight
+                    className="h-[80vh] w-[50vw] top-10 left-full"
+                    fill="purple"
+                />
+                <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+            </div>
+            <div className="h-screen w-full rounded-md dark:bg-black-100 bg-white absolute top-0 left-0 flex flex-col items-center justify-center antialiased">
+                <div className="max-w-2xl mx-auto p-4">
+                </div>
+                <BackgroundBeams />
+            </div>
+            <div className='flex justify-center relative my-20 z-10'>
+                <div className='max-w-[80vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center' >
+                    <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>
+                        Full Stack Developer
+                    </h2>
+                    <TextGenerateEffect
+                        words="Transforming Ideas Into Great User Experiences"
+                        className="text-[40px] text-center md:text-5xl lg:text-6xl"
+                    />
+                    <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
+                        Hi, I&apos;am Afaq Ibrar, Software Engineer based in Pakistan
+                    </p>
+                    <a href='#about'>
+                        <ShimmerButton title='Show My Work' icon={<FaArrowDown />} position='right' />
+                    </a>
+                </div>
+            </div>
+
         </div>
-    </div>
-  )
+    )
 }
 
 export default Hero
